b16ff226776be6b26d2d59941675931385549a8f
liuminkai <liuminkai@yeeeee.me>
2026-09-14 21:04:09 +0800

    feat: add photo works 202501日本行, 台南卯咪_202506, 想念

--- photo/manifest.js
+++ photo/manifest.js
@@ -1,5 +1,41 @@
 window.PHOTO_WORKS = [
     {
+        "slug": "202501日本行",
+        "title": "202501日本行",
+        "images": [
+            "photo/202501日本行/_DSC5647-Enhanced-NR.jpg",
+            "photo/202501日本行/_DSC5648-Enhanced-NR.jpg",
+            "photo/202501日本行/_DSC5656-Enhanced-NR.jpg",
+            "photo/202501日本行/_DSC5705-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05608-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05612-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05621-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05626-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05630-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05634-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05639-Enhanced-NR.jpg",
+            "photo/202501日本行/DSC05643-Enhanced-NR.jpg"
+        ],
+        "caption": "就日本行 沒啥好說的"
+    },
+    {
+        "slug": "台南卯咪_202506",
+        "title": "台南卯咪_202506",
+        "images": [
+            "photo/台南卯咪_202506/LMK00343-Enhanced-NR.jpg",
+            "photo/台南卯咪_202506/LMK00349-Enhanced-NR.jpg"
+        ],
+        "caption": "可愛"
+    },
+    {
+        "slug": "想念",
+        "title": "想念",
+        "images": [
+            "photo/想念/LMK00611.jpg"
+        ],
+        "caption": "Sake我好想你 離開我們快一年了呢 還是好不習慣"
+    },
+    {
         "slug": "高流和蚊子館",
         "title": "高流和蚊子館",
         "images": [
