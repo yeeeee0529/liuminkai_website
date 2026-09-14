e03feb1e2c40b8720d988f74fe363b308153685f
liuminkai <liuminkai@yeeeee.me>
2026-09-14 20:28:59 +0800

    feat: add sample photo work 高流和蚊子館

--- photo/manifest.js
+++ photo/manifest.js
@@ -1,1 +1,10 @@
-window.PHOTO_WORKS = [];
+window.PHOTO_WORKS = [
+    {
+        "slug": "高流和蚊子館",
+        "title": "高流和蚊子館",
+        "images": [
+            "photo/高流和蚊子館/DSC04456.jpg"
+        ],
+        "caption": "202608於高雄壽山觀景台拍攝"
+    }
+];
